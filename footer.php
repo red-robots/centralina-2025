7354d2a167a2233fe2538a6a75cd75f66f7c91b2
Lisa <lisaqdebona@gmail.com>
2025-02-27 02:38:56 -0500

    Styling homepage

--- footer.php
+++ footer.php
@@ -6,45 +6,66 @@
       $map_embed = get_field('map_embed','option');
       $social_media = get_field('social_media','option');
     ?>		
-    <div class="wrapper">
-      <div class="footer-columns">
-        <?php if ($contact_details) { ?>
-        <div class="footcol foot-contact-info">
-          <div class="foot-title">Contact</div>
-          <div class="foot-info">
-            <?php foreach ($contact_details as $cd) { 
-              $label = $cd['label'];
-              $description = $cd['description'];
-              $icon = $cd['icon'];
-              if($description) { ?>
-              <div class="info">
-                <?php if ($icon) { ?><?php echo $icon ?><?php } ?>
-                <?php if ($description) { ?>
-                <div class="text"><?php echo $description ?></div>
-                <?php } ?>
-              </div>
-              <?php } ?>
-            <?php } ?>
-          </div>
-        </div>
-        <?php } ?>
 
-        <?php if ($map_embed ) { ?>
-        <div class="footcol foot-contact-map">
-          <div class="mapframe">
-            <?php echo $map_embed ?>
+    <div class="footerTop">
+      <div class="wrapper">
+        <div class="footer-columns">
+          <?php if ($contact_details) { ?>
+          <div class="footcol foot-contact-info">
+            <div class="foot-title">Contact</div>
+            <div class="foot-info">
+              <?php foreach ($contact_details as $cd) { 
+                $label = $cd['label'];
+                $description = $cd['description'];
+                $icon = $cd['icon'];
+                if($description) { ?>
+                <div class="info">
+                  <?php if ($icon) { ?><?php echo $icon ?><?php } ?>
+                  <?php if ($description) { ?>
+                  <div class="text"><?php echo $description ?></div>
+                  <?php } ?>
+                </div>
+                <?php } ?>
+              <?php } ?>
+            </div>
           </div>
+          <?php } ?>
+
+          <?php if ($map_embed ) { ?>
+          <div class="footcol foot-contact-map">
+            <div class="mapframe">
+              <?php echo $map_embed ?>
+            </div>
+          </div>
+          <?php } ?>
         </div>
+        <?php if ($social_media) { ?>
+        <div class="foot-social-media">
+          <div class="foot-title">Follow</div>
+          <?php foreach ($social_media as $sm) { ?>
+            
+          <?php } ?>
+        </div>    
         <?php } ?>
       </div>
-      <?php if ($social_media) { ?>
-      <div class="foot-social-media">
-        <div class="foot-title">Follow</div>
-        <?php foreach ($social_media as $sm) { ?>
-          
-        <?php } ?>
-      </div>    
-      <?php } ?>
+    </div>
+
+    <?php 
+      $copyright = get_field('copyright', 'option'); 
+      $footer_logos = get_field('footer_logos', 'option'); 
+    ?>
+    <div class="copyright-section">
+      <div class="wrapper">
+        <div class="flexwrap">
+          <?php if ($copyright) { ?>
+          <div class="copyright">
+              <?php echo $copyright ?>
+            <div class="poweredby">Site by <a href="https://bellaworksweb.com/" target="_blank">Bellaworks</a></div>      
+          </div>
+          <?php } ?>
+          <div class="footer-logo"></div>
+        </div>
+      </div>
     </div>
 	</footer><!-- #colophon -->
 	
