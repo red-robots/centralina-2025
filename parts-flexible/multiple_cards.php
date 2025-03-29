97f63454f1e10894e15b4b15525a410496e4505f
Lisa <lisaqdebona@gmail.com>
2025-03-29 03:18:48 -0400

    Added ACF sections on internal page

--- parts-flexible/multiple_cards.php
+++ parts-flexible/multiple_cards.php
@@ -13,34 +13,65 @@
         <div class="intro"><?php echo anti_email_spam($intro); ?></div>  
         <?php } ?>
 
-        <?php if ($phonebook) { ?>
-        <div class="infoCards">
-          <?php foreach ($phonebook as $p) { 
-            $pid = $p->ID;
-            $title = $p->post_title;
-            $company = get_field('company', $pid);
-            $phone = get_field('phone', $pid);
-            if($title || $text) { ?>
-            <div class="infoCard">
-              <div class="inside">
-                <div class="textwrap">
-                  <div class="top">
-                    <?php if ($title) { ?>
-                    <div class="infoTitle"><?php echo $title ?></div>  
-                    <?php } ?>
-                    <?php if ($company) { ?>
-                    <div class="infoText"><?php echo anti_email_spam($company); ?></div>  
+        <?php if ($card_option=='phonebook') { ?>
+          <?php if ($phonebook) { ?>
+          <div class="infoCards card-phonebook">
+            <?php foreach ($phonebook as $p) { 
+              $pid = $p->ID;
+              $title = $p->post_title;
+              $company = get_field('company', $pid);
+              $phone = get_field('phone', $pid);
+              if($title || $company) { ?>
+              <div class="infoCard">
+                <div class="inside">
+                  <div class="textwrap">
+                    <div class="top">
+                      <?php if ($title) { ?>
+                      <div class="infoTitle"><?php echo $title ?></div>  
+                      <?php } ?>
+                      <?php if ($company) { ?>
+                      <div class="infoText"><?php echo anti_email_spam($company); ?></div>  
+                      <?php } ?>
+                    </div>
+                    <?php if ($phone) { ?>
+                    <div class="infoBottom"><?php echo $phone ?></div>  
                     <?php } ?>
                   </div>
-                  <?php if ($phone) { ?>
-                  <div class="infoBottom"><?php echo $phone ?></div>  
-                  <?php } ?>
                 </div>
               </div>
-            </div>
+              <?php } ?>
             <?php } ?>
+          </div>  
           <?php } ?>
-        </div>  
+        <?php } else { ?>
+          <?php if ($cards) { ?>
+          <div class="infoCards card-custom">
+            <?php foreach ($cards as $p) { 
+              $title = $p['title'];
+              $text = $p['text'];
+              $phone = $p['text_bottom'];;
+              if($title || $text) { ?>
+              <div class="infoCard">
+                <div class="inside">
+                  <div class="textwrap">
+                    <div class="top">
+                      <?php if ($title) { ?>
+                      <div class="infoTitle"><?php echo $title ?></div>  
+                      <?php } ?>
+                      <?php if ($text) { ?>
+                      <div class="infoText"><?php echo anti_email_spam($text); ?></div>  
+                      <?php } ?>
+                    </div>
+                    <?php if ($phone) { ?>
+                    <div class="infoBottom"><?php echo $phone ?></div>  
+                    <?php } ?>
+                  </div>
+                </div>
+              </div>
+              <?php } ?>
+            <?php } ?>
+          </div>  
+          <?php } ?>
         <?php } ?>
       </div>
     </div>
