d2c35d3b597504c15efc5051f3f97457d19c877b
Lisa <lisaqdebona@gmail.com>
2025-04-19 02:59:03 -0400

    Added repeater fields for Volunteer page

--- parts-flexible/two_column_text_image.php
+++ parts-flexible/two_column_text_image.php
@@ -1,7 +1,10 @@
 <?php if( get_row_layout() == 'two_column_text_image' ) {
+  $image_position = get_sub_field('image_position');
+  $image_position = ($image_position) ? ' image-' . $image_position : ' image-right';
   $text = get_sub_field('text');
   $image = get_sub_field('image');
   $columnClass = ($text && $image) ? 'half' : 'full';
+  $columnClass .= $image_position;
   $disable_section = get_sub_field('disable_section');
   $is_visible = ($disable_section) ? false : true;
   if( $is_visible && $text ) { ?>
