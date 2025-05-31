25a1aadcd65cb4babd2f5b914b891d10656facf9
Lisa <lisaqdebona@gmail.com>
2025-05-31 01:25:15 -0400

    Updated flexible content template

--- inc/theme.php
+++ inc/theme.php
@@ -65,7 +65,10 @@ add_filter( 'login_headertext', 'bella_login_logo_url_title' );
 //   acf_add_options_page($settings);
 // }
 add_action('acf/init', function() {
-  if( function_exists('acf_add_options_page') ) {acf_add_options_page();}
+  if( function_exists('acf_add_options_page') ) {
+    $settings['page_title'] = __('Global Options');
+    acf_add_options_page($settings);
+  }
 });
 
 /*-------------------------------------
